d4885830319206c7f762778de2901d32a5990bc6
Björn Schuller <>
2022-02-04 14:40:17 +0100

    video6 static image

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,5 @@ module.exports = {
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
-  plugins: [
-    
-  ],
+  plugins: [`gatsby-plugin-image`, `gatsby-plugin-sharp`],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import * as React from "react"
 import Layout from "../components/layout"
-import Navbar from "../components/navbar"
+import { StaticImage } from "gatsby-plugin-image"
+import * as classes from "../styles/index.module.css"
 
 const IndexPage = () => (
   <Layout>
-    <h1>Eat, Sleep</h1>
-    <h2>Code & Repeat</h2>
-    <p>Software developer who likes to eat and sleep</p>
+    <div className={classes.IndexPage}>
+      <div className={classes.Intro}>
+        <h1>Eat, Sleep</h1>
+        <h2>Code & Repeat</h2>
+        <p>Software developer who likes to eat and sleep</p>
+      </div>
+      <StaticImage alt="laptop" src="../images/laptop.png" width={1100} />
+    </div>
   </Layout>
 )
 
